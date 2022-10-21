b8088e9eeaac675330480aa64760ee211de0c000
EgVazquez <evazquez0691@gmail.com>
2022-10-20 23:19:09 -0300

    Se corrigieron por menores

--- tp2marquez/ScriptCrearTablas.sql
+++ tp2marquez/ScriptCrearTablas.sql
@@ -1,50 +1,52 @@
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema Libreria
+-- Schema libreria
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema Libreria
+-- Schema libreria
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Libreria` DEFAULT CHARACTER SET utf8 ;
-USE `Libreria` ;
+CREATE SCHEMA IF NOT EXISTS `LIBRERIA` DEFAULT CHARACTER SET UTF8MB4;
+
+USE `LIBRERIA`;
 
 -- -----------------------------------------------------
--- Table `Libreria`.`usuarios`
+-- Table `libreria`.`usuarios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Libreria`.`usuarios` (
-  `idusuarios` INT(11) NOT NULL AUTO_INCREMENT,
-  `nombreusuario` VARCHAR(45) NOT NULL,
-  `clave` VARCHAR(45) NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  `apellido` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idusuarios`))
-ENGINE = InnoDB
-ROW_FORMAT = DYNAMIC;
+CREATE TABLE IF NOT EXISTS `LIBRERIA`.`USUARIOS` (
+  `IDUSUARIOS` INT(11) NOT NULL AUTO_INCREMENT,
+  `NOMBREUSUARIO` VARCHAR(45) NOT NULL,
+  `CLAVE` VARCHAR(45) NOT NULL,
+  `NOMBRE` VARCHAR(45) NOT NULL,
+  `APELLIDO` VARCHAR(45) NOT NULL,
+  `EMAIL` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`IDUSUARIOS`)
+) ENGINE = INNODB DEFAULT CHARACTER SET = UTF8 ROW_FORMAT = DYNAMIC;
 
-
 -- -----------------------------------------------------
--- Table `Libreria`.`libros`
+-- Table `libreria`.`libros`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Libreria`.`libros` (
+CREATE TABLE IF NOT EXISTS `LIBRERIA`.`LIBROS` (
   `ISBN` INT(11) NOT NULL AUTO_INCREMENT,
-  `titulo` VARCHAR(45) NOT NULL,
-  `autor` VARCHAR(45) NOT NULL,
-  `genero` VARCHAR(45) NOT NULL,
-  `usuarioalquila` INT(11) NULL,
+  `TITULO` VARCHAR(45) NOT NULL,
+  `AUTOR` VARCHAR(45) NOT NULL,
+  `GENERO` VARCHAR(45) NOT NULL,
+  `ALQUILADO` BOOLEAN NULL DEFAULT FALSE,
+  `USUARIOALQUILA` INT(45) NULL DEFAULT NULL,
   PRIMARY KEY (`ISBN`),
-  INDEX `fk_libros_usuarios_idx` (`usuarioalquila`),
-  CONSTRAINT `fk_libros_usuarios`
-    FOREIGN KEY (`usuarioalquila`)
-    REFERENCES `Libreria`.`usuarios` (`idusuarios`)
-)
-ENGINE = InnoDB;
+  INDEX `FK_LIBROS_USUARIOS_IDX` (`USUARIOALQUILA` ASC) VISIBLE,
+  CONSTRAINT `USUARIOALQUILA` FOREIGN KEY (`USUARIOALQUILA`) REFERENCES `LIBRERIA`.`USUARIOS` (`IDUSUARIOS`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = INNODB AUTO_INCREMENT = 2 DEFAULT CHARACTER SET = UTF8;
 
 SET SQL_MODE=@OLD_SQL_MODE;
+
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
